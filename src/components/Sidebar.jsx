9e6a9de551406acb7256f13e408f700dc5c67485
soundwanders <soundwanders@gmail.com>
2022-03-08 07:52:48 -0500

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -55,7 +55,7 @@ const Container = styled.div`
   background-color: #0e1e36;
   gap: 3rem;
   @media screen and (min-width: 320px) and (max-width: 1080px) {
-    width: 98%;
+    width: 99%;
     height: max-content !important;
     justify-content: center;
     margin: 0;
